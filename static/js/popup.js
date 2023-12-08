b8e0222c81a5a2c6d378e2a538358aa34c846ac1
sctop <2094880085@qq.com>
2023-12-08 23:36:33 +0800

    CN popup added; popup js modified

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,5 +1,8 @@
 var current_popup = void 0;
 
+function setScrollToNone() {document.getElementsByTagName("html")[0].style = "";}
+function setScrollToSmooth() {document.getElementsByTagName("html")[0].style = "scroll-behavior: smooth;";}
+
 function removePopup(popup) {
     popup.hide();
     current_popup.popupEl.remove();
@@ -13,10 +16,10 @@ function showPopupByTooltipId(tooltip_id) {
     current_popup = new Popup({"title": "  ",
         "content": tooltip_data[tooltip_id],
         hideCallback: () => {
-            document.getElementsByTagName("html")[0].style = "scroll-behavior: smooth;";
+            setScrollToSmooth();
         },
     });
-    document.getElementsByTagName("html")[0].style = "";
+    setScrollToNone();
     current_popup.show();
     display_zhcn_element();
 }
