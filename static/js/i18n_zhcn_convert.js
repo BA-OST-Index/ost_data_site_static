b1cc48008f447119a7fa5795db04a9f031920c88
sctop <2094880085@qq.com>
2024-06-16 01:38:33 +0800

    fix: Story icon CSS修复；zh_cn转换加入title元素

--- static/js/i18n_zhcn_convert.js
+++ static/js/i18n_zhcn_convert.js
@@ -11,6 +11,61 @@ async function initZhcnConversionDB() {
     }
 }
 
+function _convertZhcn(element) {
+    const currElement = element;
+
+    // save the original content to a data attribute if not
+    // (e.g. first time being inited)
+    if (currElement.dataset.text === undefined) {
+        let _innerHTML = currElement.innerHTML;
+        _innerHTML = _innerHTML.replaceAll("'", "[single_quote]");
+        currElement.dataset.text = encodeURIComponent(_innerHTML);
+    }
+
+    // ChatGPT generated the following code from here
+    // and I modified to it to suit
+    let _mode = localStorage.getItem("zh_cn");
+    switch (_mode) {
+        case "zh_cn_jp":
+            _mode = 0;
+            break;
+        case "zh_cn_cn":
+            _mode = 1;
+            break;
+        case "zh_cn_tw":
+            _mode = 2;
+            break;
+        case "all":
+            _mode = 3;
+            break;
+        default:
+            _mode = 0;
+    }
+
+    let temp = decodeURIComponent(currElement.dataset.text);
+    temp = temp.replaceAll("&amp;", "&");
+    temp = temp.replaceAll("[single_quote]", "'");
+
+    for (const [key, values] of Object.entries(ZHCN_CONVERSION_DB)) {
+        for (const entry of values) {
+            for (const i of entry) {
+                const searchStr = "{" + key + i + "}";
+
+                let replaceStr;
+                if (_mode === 3) {
+                    replaceStr = entry[0] + " (zh_cn_jp) / " + entry[1] + " (zh_cn_cn) / " + entry[2] + " (zh_cn_jp) "
+                }
+                else {
+                    replaceStr = entry[_mode];
+                }
+
+                temp = temp.replaceAll(searchStr, replaceStr);
+            }
+        }
+    }
+    currElement.innerHTML = temp;
+}
+
 function convertZhcn() {
     let allZhcnElements = document.getElementsByClassName("i18n-zhcn-text");
 
@@ -21,56 +76,7 @@ function convertZhcn() {
 
     for (let i = 0; i < allZhcnElements.length; i++) {
         const currElement = allZhcnElements[i];
-
-        // save the original content to a data attribute if not
-        // (e.g. first time being inited)
-        if (currElement.dataset.text === undefined) {
-            let _innerHTML = currElement.innerHTML;
-            _innerHTML = _innerHTML.replaceAll("'", "[single_quote]");
-            currElement.dataset.text = encodeURIComponent(_innerHTML);
-        }
-
-        // ChatGPT generated the following code from here
-        // and I modified to it to suit
-        let _mode = localStorage.getItem("zh_cn");
-        switch (_mode) {
-            case "zh_cn_jp":
-                _mode = 0;
-                break;
-            case "zh_cn_cn":
-                _mode = 1;
-                break;
-            case "zh_cn_tw":
-                _mode = 2;
-                break;
-            case "all":
-                _mode = 3;
-                break;
-            default:
-                _mode = 0;
-        }
-
-        let temp = decodeURIComponent(currElement.dataset.text);
-        temp = temp.replaceAll("&amp;", "&");
-        temp = temp.replaceAll("[single_quote]", "'");
-
-        for (const [key, values] of Object.entries(ZHCN_CONVERSION_DB)) {
-            for (const entry of values) {
-                for (const i of entry) {
-                    const searchStr = "{" + key + i + "}";
-
-                    let replaceStr;
-                    if (_mode === 3) {
-                        replaceStr = entry[0] + " (zh_cn_jp) / " + entry[1] + " (zh_cn_cn) / " + entry[2] + " (zh_cn_jp) "
-                    }
-                    else {
-                        replaceStr = entry[_mode];
-                    }
-
-                    temp = temp.replaceAll(searchStr, replaceStr);
-                }
-            }
-        }
-        currElement.innerHTML = temp;
+        _convertZhcn(currElement);
     }
+    _convertZhcn(document.getElementsByTagName("title")[0]);
 }
